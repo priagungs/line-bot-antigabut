a0f505aef887b2294ae7004620fbf543a391a195
priagungs <32959030+priagungs@users.noreply.github.com>
2018-02-23 08:11:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,4 +44,5 @@ def handle_text_message(event):
 
 import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
