c021657def35c8e0cae87ad1777914a08b581710
priagungs <32959030+priagungs@users.noreply.github.com>
2018-02-23 21:05:51 +0700

    updated

--- bot.py
+++ bot.py
@@ -38,17 +38,42 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text #message from user
-    if(text == 'profile'):
-        user_profile = line_bot_api.get_profile(event.source.user_id)
+    if(text.split()[0] == '!profile'):
+        if(len(text.split()) == 1):
+            user_profile = line_bot_api.get_profile(event.source.user_id)
+        else:
+            if(isinstance(event.source, SourceRoom)):
+                list_member = line_bot_api.get_room_member_ids(event.source.room_id)
+            elif(isinstance(event.source, SourceGroup)):
+                list_member = line_bot_api.get_group_member_ids(event.source.group_id)
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(
+                        text = 'me neither inside a group nor room chat :('
+                    )
+                )
+                return
+            member_name = text.split('!profile')[1]
+            for mem_id in list_member.member_ids:
+                if(member_name.lower() in line_bot_api.get_profile(mem_id).display_name):
+                    user_profile = line_bot_api.get_profile(mem_id)
+                    found = True
+                    break
+            if not found:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(
+                        text = 'sorry, no member has name ' + member_name
+                    )
+                )
+                return
+
         text_message = TextSendMessage(text =
             'Nama : ' + user_profile.display_name +
             '\nStatus : ' + user_profile.status_message +
             '\nPicture : ' + user_profile.picture_url)
         line_bot_api.reply_message(event.reply_token, text_message)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text = 'group id = ' + str(event.source.group_id))
-        )
 
 
 import os
