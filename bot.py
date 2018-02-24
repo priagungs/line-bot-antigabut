8f897f436c45904627b272a35f390dd14b026535
priagungs <32959030+priagungs@users.noreply.github.com>
2018-02-24 14:32:06 +0700

    updated

--- bot.py
+++ bot.py
@@ -21,6 +21,10 @@ from linebot.models import (
 
 import time
 
+kata_kasar = [
+    'ajg', 'bgst', 'kampret', 'anjing', 'bangsat', 'qntl', 'kontol', 'ngentot', 'ngentiaw', 'bct', 'bacot', 'anjir', 'jir', 'anjay'
+]
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('WRwK6zLY9ZJzx9ak/ymI8ez/n+rUgzJeUC+rkWGC9BVvgxaUqKtIpMYmQtYeT+9gqDoVIxeyX8x/XLYV4N194aHJyON2PXPvTFFKF8InxzgrwsnakZm/PPNy14YKKNMYdcl77pbWN/Th86ryTOh9ZQdB04t89/1O/w1cDnyilFU=') #Your Channel Access Token
@@ -46,56 +50,25 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text #message from user
-    if(text.split()[0] == '!profile'):
-        if(len(text.split()) == 1):
-            user_profile = line_bot_api.get_profile(event.source.user_id)
-        else:
-            if(isinstance(event.source, SourceRoom)):
-                list_member = line_bot_api.get_room_member_ids(event.source.room_id)
-            elif(isinstance(event.source, SourceGroup)):
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(
-                        text = 'masuk bray1'
-                    )
-                )
-                list_member = line_bot_api.get_group_member_ids(event.source.group_id)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(
-                        text = 'masuk bray2'
-                    )
-                )
-            else:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(
-                        text = 'Not in a group or room chat!'
-                    )
-                )
-                return
-
-            member_name = text.split('!profile ')
-            for mem_id in list_member.member_ids:
-                if(member_name.lower() in line_bot_api.get_group_member_profile(mem_id).display_name):
-                    user_profile = line_bot_api.get_group_member_profile(mem_id)
-                    found = False
-                    break
-            if not found:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(
-                        text = 'sorry, no member has name ' + member_name
-                    )
-                )
-                return
-
+    if(text == '!profile'):
+        user_profile = line_bot_api.get_profile(event.source.user_id)
         text_message = TextSendMessage(text =
             'Nama : ' + user_profile.display_name +
             '\nStatus : ' + user_profile.status_message +
             '\nPicture : ' + user_profile.picture_url)
         line_bot_api.reply_message(event.reply_token, text_message)
 
+    elif(text.split()[0] == '!gabut' and len(text.split() >=2)):
+        text_message = TextSendMessage(
+            text = 'Gabut maneh ' + text.split('!gabut ')[1]
+        )
+        line_bot_api.reply_message(event.reply_token, text_message)
+    else:
+        text = text.split()
+        for word in text:
+            if word in kata_kasar:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = 'ih ga boleh ngomong kasar :('))
+                break
 
 import os
 if __name__ == "__main__":
